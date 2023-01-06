53a13076e3d010b567e42bc647cdbddffa6159a3
Jxl-s <77560326+Jxl-s@users.noreply.github.com>
2023-01-05 23:16:15 -0500

    modified release script

--- release.sh
+++ release.sh
@@ -1,17 +1,19 @@
-# 1. Run the build scripts
-cargo build --release;
-cargo build --release --target x86_64-pc-windows-gnu;
-CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc \
-cargo build --release --target x86_64-unknown-linux-gnu;
+RELEASE_VERSION="0.1.1"
 
-# 2. Create the release directory
+# Create the release directory
 if [ -d "release" ]; then
     rm -rf release
 fi
 
 mkdir release
 
-# 3. Move the binaries to the release directory
-mv target/release/luajoin release/luajoin-aarch64-apple-darwin
-mv target/x86_64-pc-windows-gnu/release/luajoin.exe release/luajoin-x86_64-pc-windows-gnu.exe
-mv target/x86_64-unknown-linux-gnu/release/luajoin release/luajoin-x86_64-unknown-linux-gnu
+# Run the build scripts
+cargo build --release;
+cargo build --release --target x86_64-pc-windows-gnu;
+CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER=x86_64-unknown-linux-gnu-gcc \
+cargo build --release --target x86_64-unknown-linux-gnu;
+
+# 3. Zip the binaries, in the format luajoin-<platform>-<version>.zip
+zip release/luajoin-macos-$RELEASE_VERSION.zip target/release/luajoin
+zip release/luajoin-linux-$RELEASE_VERSION.zip target/x86_64-unknown-linux-gnu/release/luajoin
+zip release/luajoin-win64-$RELEASE_VERSION.zip target/x86_64-pc-windows-gnu/release/luajoin.exe
